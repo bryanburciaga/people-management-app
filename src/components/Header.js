3ce6ef88912c03eeeb6319ccfe28912dba73a414
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-09-26 11:32:09 -0600

    firebase added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,25 @@
+import { login, logout } from '../firebase';
 import { Link } from 'react-router-dom';
 
-function Header(props) {
+function Header({ user }) {
     return (
-        <>
         <nav className="nav">
-            <Link to={`/`}>
+            <Link to="/">
                 <div>People App</div>
             </Link>
+            <section className="auth-options">
+                {
+                    user ?
+                    <>
+                        <div>Welcome, {user.displayName}</div>
+                        <div onClick={logout}>Logout</div>
+                    </>
+                    :
+                    <div onClick={login}>Login</div>
+                }
+            </section>
         </nav>
-        </>
-    )
+    );
 }
 
 export default Header;
